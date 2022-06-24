cfb7d3bf4ec002baaf2a694686e04ace51c498f0
lostoneway <abishop416@gmail.com>
2022-06-23 20:14:11 -0500

    variables and MongoConnection

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+//variables 
+let db,    
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'star-wars-quotes',
+    collection
+
+    //connect to db
+MongoClient.connect(dbConnectionString) 
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
